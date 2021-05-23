50adb29d55d0c4eee33d97a4316a73fcca1ffb7d
vangel1227 <vangel1227@gmail.com>
2021-05-23 18:52:59 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/recipe-app-ng'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/recipe-app-ng/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
